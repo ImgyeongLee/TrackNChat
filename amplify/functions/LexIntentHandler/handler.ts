06e8b1c296c31bd9fc4807a78ab687e30333f736
Toby Loki <tobyloki5@gmail.com>
2024-05-26 15:09:09 -0700

    updated LexIntentHandler to return response in proper format

--- amplify/functions/LexIntentHandler/handler.ts
+++ amplify/functions/LexIntentHandler/handler.ts
@@ -2,7 +2,20 @@ import { Handler } from 'aws-lambda';
 
 export const handler: Handler = async (event, context) => {
 	console.log(JSON.stringify(event, null, 2));
-	console.log('hi there');
-	// your function code goes here
-	return 'Hello, World!';
+
+	const sessionState = event.sessionState;
+	sessionState.intent.state = 'Fulfilled';
+	sessionState.dialogAction = {
+		type: 'Close'
+	};
+
+	return {
+		sessionState,
+		messages: [
+			{
+				contentType: 'PlainText',
+				content: `Hello there! I received this prompt: ${event.inputTranscript}`
+			}
+		]
+	};
 };
