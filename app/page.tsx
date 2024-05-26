5135a892ea4338433d3549710d828d3c7db68c52
ImgyeongLee <mingbab33@gmail.com>
2024-05-26 13:43:25 -0700

    feat: frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,40 @@
-"use client";
+'use client';
 
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
-import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
-import "@aws-amplify/ui-react/styles.css";
+import { useState, useEffect } from 'react';
+import './../app/app.css';
+import { Amplify } from 'aws-amplify';
+import outputs from '@/amplify_outputs.json';
+import '@aws-amplify/ui-react/styles.css';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import './globals.css';
 
 Amplify.configure(outputs);
 
-const client = generateClient<Schema>();
-
 export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
-
-  useEffect(() => {
-    listTodos();
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
-
-  return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
-    </main>
-  );
+    const [chats, setChats] = useState<string[]>([]);
+    return (
+        <main className="h-full w-full flex flex-col justify-center items-center">
+            <Card className="w-[calc(250px+30vw)]">
+                <CardHeader>
+                    <CardTitle>Ask your question!</CardTitle>
+                    <CardDescription>You can type / to see the other commands.</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <div className="bg-slate-200 w-full min-h-[50vh] max-h-[50vh] overflow-y-auto p-2 flex flex-col items-center">
+                        <div className="rounded bg-white text-[#0e1926] py-2 px-3 self-end">Hello.</div>
+                        <div className="rounded bg-[#0e1926] text-white py-2 px-3 self-start">
+                            I don't understand what you said
+                        </div>
+                    </div>
+                </CardContent>
+                <CardFooter className="flex justify-between space-x-2">
+                    <Input type="text" placeholder="Type your tracking number..." />
+                    <Button>Send</Button>
+                </CardFooter>
+            </Card>
+            <footer>Need any guidelines? Scroll Down!</footer>
+        </main>
+    );
 }
